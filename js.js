62dd025c5a6ac9f412b6268b07008c932ea7660e
minamiguchi <kazu9sum.mi@gmail.com>
2023-11-15 14:03:25 +0900

    add feature - keyboard shortcut - check auto translate

--- js.js
+++ js.js
@@ -6,17 +6,19 @@ $(function(){
         }
     });
 
-
-    $("#ja-textarea").keydown(function(e) {
-        if(e.altKey){
-            if(e.code==="Enter"){
-                translate();
-            }
-        }
-    })
-
     $("#btn-translate").on("click", function() {
         translate();
+    })
+
+
+    $(window).keydown(function(e) {
+        if(e.ctrlKey){
+            if(e.code==="Enter"){ //Ctrl + Enter
+                translate();
+            }else if(e.code==="KeyB"){ //Ctrl + b
+                $("#autoTranslate").prop("checked", !$("#autoTranslate").prop("checked"))
+            }
+        }
     })
 
 });
@@ -26,10 +28,12 @@ function translate(){
     if(text!==""){
         const formatText = encodeURI(text.replace(/\//g,"／").replace(/#/g,"＃").replace(/\r?\n/g," "));
         const url = "https://www.deepl.com/translator#en/ja/"+formatText
-        // console.log(url)
         window.open(url);
         setTimeout(function(){
+            $("#autoTranslate").prop("checked", true)
             $("#ja-textarea").val("");
         },1000);
+    }else{
+
     }
 }
