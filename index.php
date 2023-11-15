a7b106f56da1d6d273f9c03fd3425cf810f3662e
minamiguchi <kazu9sum.mi@gmail.com>
2023-11-15 14:32:47 +0900

    add gtag

--- index.php
+++ index.php
@@ -10,8 +10,19 @@
     <script src="https://code.jquery.com/jquery-3.6.0.min.js" integrity="sha256-/xUj+3OJU5yExlq6GSYGSHk7tPXikynS7ogEvDej/m4=" crossorigin="anonymous"></script>
     <script src="js.js"></script>
     <title>翻訳くん</title>
+<!-- Google Tag Manager -->
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-MKGFTM8N');</script>
+<!-- End Google Tag Manager -->
 </head>
 <body>
+<!-- Google Tag Manager (noscript) -->
+<noscript><iframe src="https://www.googletagmanager.com/ns.html?id=GTM-MKGFTM8N"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<!-- End Google Tag Manager (noscript) -->
     <article>
         <textarea name="" id="ja-textarea" cols="30" rows="10" class="textarea" autofocus></textarea>
         <div id="btn-translate">翻訳する (Ctrl+Enter)</div>
